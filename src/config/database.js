fffa1d96626cd64e474a2a95f32ebded39068b5b
Allan Gondin <allangondin@gmail.com>
2020-06-23 20:33:38 -0300

    até aqui funciona

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'localhost', 
+    database: 'fast_feet',
+    username: 'root',
+    password: 'root',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    }   
+}
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController'
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-    return res.json({ message: 'Hello World Alone'});
-})
+routes.post('/user', UserController.store);
 
 
 export default routes;
